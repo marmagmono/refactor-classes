d861fd83891aed04445286e9447f61509f5b5feb
marmagmono <marmagmono@gmail.com>
2019-02-05 21:38:11 +0100

    Parameter list refactoring - preparation.

---
+++ src/RefactorClasses.Test/ParameterList/ParameterListRefactoringTest.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestHelper;
+
+namespace RefactorClasses.Test.ParameterList
+{
+    [TestClass]
+    public class ParameterListRefactoringTest : DiagnosticVerifier
+    {
+        [TestMethod]
+        public async Task SingleLineParameterList_IsConvertedToMultilineParameterList()
+        {
+            // Arrange
+            var testString = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+internal enum AnEnum1
+{
+    FirstThing,
+    SecondThing = 2
+}
+
+internal class Class3<T> where T : class
+{
+    public Class3(AnEnum1 enumProp, int klo, T aaa)
+    {
+        EnumProp = enumProp;
+        Klo = klo != 0 ? klo : throw new Exception();
+        Prop1 = aaa ?? throw new NullReferenceException();
+    }
+}
+";
+            var expectedText = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+internal enum AnEnum1
+{
+    FirstThing,
+    SecondThing = 2
+}
+
+internal class Class3<T> where T : class
+{
+    public Class3(
+        AnEnum1 enumProp,
+        int klo,
+        T aaa)
+    {
+        EnumProp = enumProp;
+        Klo = klo != 0 ? klo : throw new Exception();
+        Prop1 = aaa ?? throw new NullReferenceException();
+    }
+}
+";
+
+            CodeAction registeredAction = null;
+            var document = CreateDocument(testString);
+            var context = CreateRefactoringContext(document, new TextSpan(250, 0), a => registeredAction = a);
+            var sut = CreateSut();
+
+            // Act
+            await sut.ComputeRefactoringsAsync(context);
+            Assert.IsNotNull(registeredAction);
+
+            var changedDocument = await ApplyRefactoring(document, registeredAction);
+            var changedText = (await changedDocument.GetTextAsync()).ToString();
+
+            // Assert
+            Assert.AreEqual(expectedText, changedText);
+        }
+
+        [TestMethod]
+        public async Task MultilineParameterList_IsConvertedToSingleLineParameterList()
+        {
+            // Arrange
+            var testString = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+internal enum AnEnum1
+{
+    FirstThing,
+    SecondThing = 2
+}
+
+internal class Class3<T> where T : class
+{
+    public Class3(
+        AnEnum1 enumProp,
+        int klo,
+        T aaa)
+    {
+        EnumProp = enumProp;
+        Klo = klo != 0 ? klo : throw new Exception();
+        Prop1 = aaa ?? throw new NullReferenceException();
+    }
+}
+";
+
+            var expectedText = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+internal enum AnEnum1
+{
+    FirstThing,
+    SecondThing = 2
+}
+
+internal class Class3<T> where T : class
+{
+    public Class3(AnEnum1 enumProp, int klo, T aaa)
+    {
+        EnumProp = enumProp;
+        Klo = klo != 0 ? klo : throw new Exception();
+        Prop1 = aaa ?? throw new NullReferenceException();
+    }
+}
+";
+
+            CodeAction registeredAction = null;
+            var document = CreateDocument(testString);
+            var context = CreateRefactoringContext(document, new TextSpan(262, 0), a => registeredAction = a);
+            var sut = CreateSut();
+
+            // Act
+            await sut.ComputeRefactoringsAsync(context);
+            Assert.IsNotNull(registeredAction);
+
+            var changedDocument = await ApplyRefactoring(document, registeredAction);
+            var changedText = (await changedDocument.GetTextAsync()).ToString();
+
+            // Assert
+            Assert.AreEqual(expectedText, changedText);
+        }
+
+        public async Task<Document> ApplyRefactoring(Document originalDocument, CodeAction codeAction)
+        {
+            var operations = await codeAction.GetOperationsAsync(default(CancellationToken));
+            var solution = operations.OfType<ApplyChangesOperation>().Single().ChangedSolution;
+            return solution.GetDocument(originalDocument.Id);
+        }
+
+        private CodeRefactoringContext CreateRefactoringContext(
+            Document document,
+            TextSpan textSpan,
+            Action<CodeAction> registerRefactoring) =>
+        new CodeRefactoringContext(
+            document,
+            textSpan,
+            registerRefactoring,
+            default(CancellationToken));
+
+        private CodeRefactoringContext CreateRefactoringContext(
+            string documentText,
+            TextSpan textSpan,
+            Action<CodeAction> registerRefactoring) =>
+                new CodeRefactoringContext(
+                    CreateDocument(documentText),
+                    textSpan,
+                    registerRefactoring,
+                    default(CancellationToken));
+
+        private RefactorClasses.ParameterListRefactoring.RefactoringProvider CreateSut() =>
+            new RefactorClasses.ParameterListRefactoring.RefactoringProvider();
+    }
+}
---
+++ src/RefactorClasses/ParameterListRefactoring/RefactoringProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RefactorClasses.CodeActions;
+using RefactorClasses.CodeRefactoringUtils;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorClasses.ParameterListRefactoring
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = "ParameterListRefactroing"), Shared]
+    public class RefactoringProvider : CodeRefactoringProvider
+    {
+        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            var (document, parameterList) = await context.FindSyntaxForCurrentSpan<ParameterListSyntax>();
+            if (document == null || parameterList == null) return;
+
+            var isMultiline = parameterList.OpenParenToken.TrailingTrivia.Any(SyntaxKind.EndOfLineTrivia);
+            if (isMultiline)
+            {
+                context.RegisterRefactoring(new DelegateCodeAction(
+                    "Make parameters single line",
+                    c => MakeSingleLine(document, parameterList)));
+            }
+            else
+            {
+                context.RegisterRefactoring(new DelegateCodeAction(
+                    "Split parameters into multiple lines",
+                    c => MakeMultiline(document, parameterList)));
+            }
+        }
+
+        private static async Task<Document> MakeMultiline(Document document, ParameterListSyntax parameterList)
+        {
+            return document;
+        }
+
+        private static async Task<Document> MakeSingleLine(Document document, ParameterListSyntax parameterList)
+        {
+            return document;
+        }
+    }
+}
